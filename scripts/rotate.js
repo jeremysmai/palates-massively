d78110e24acb931378ad1797bd20826f98fcdb7b
JDizzle <jdizzle@JDizzles-MacBook-Pro.local>
2018-08-05 23:46:44 -0500

    final image inbound

--- scripts/rotate.js
+++ scripts/rotate.js
@@ -1,6 +1,24 @@
 window.onload = choosePic;
 
-var myPix = new Array("images/rotate/PP1-1.JPG","images/rotate/PP1-2.JPG","images/rotate/PP1-3.JPG","images/rotate/PP1-4.JPG","images/rotate/PP1-5.JPG","images/rotate/PP2-1.JPG","images/rotate/PP2-2.JPG","images/rotate/PP2-3.JPG","images/rotate/PP2-4.JPG","images/rotate/PP2-5.JPG","images/rotate/PP2-6.JPG","images/rotate/PP2-7.JPG","images/rotate/PP2-8.JPG","images/rotate/PP2-9.JPG","images/rotate/PP3-1.JPG","images/rotate/PP3-2.JPG","images/rotate/PP3-3.JPG");
+var myPix = new Array("images/rotate/PP1-1.JPG",
+                      "images/rotate/PP1-2.JPG",
+                      "images/rotate/PP1-3.JPG",
+                      "images/rotate/PP1-4.JPG",
+                      "images/rotate/PP1-5.JPG",
+                      "images/rotate/PP2-1.JPG",
+                      "images/rotate/PP2-2.JPG",
+                      "images/rotate/PP2-3.JPG",
+                      "images/rotate/PP2-4.JPG",
+                      "images/rotate/PP2-5.JPG",
+                      "images/rotate/PP2-6.JPG",
+                      "images/rotate/PP2-7.JPG",
+                      "images/rotate/PP2-8.JPG",
+                      "images/rotate/PP2-9.JPG",
+                      "images/rotate/PP3-1.JPG",
+                      "images/rotate/PP3-2.JPG",
+                      "images/rotate/PP3-3.JPG",
+                      "images/rotate/PP3-4.JPG"
+                      );
 
 function choosePic() {
      var randomNum = Math.floor(Math.random() * myPix.length);
